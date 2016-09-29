c7ac6c93a79f30299e3f0a8a6d458e2fcb58a6b8
Jxx <27117821@qq.com>
2016-09-29 18:27:29 +0800

    init

---
+++ app/src/main/java/com/jxx/weatherforecast/activity/BaseActivity.java
@@ -0,0 +1,32 @@
+package com.jxx.weatherforecast.activity;
+
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jxx.weatherforecast.R;
+
+public class BaseActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public String getWeatherKey(){
+        String keyvalue = null;
+        try {
+            ApplicationInfo applicationInfo = this.getPackageManager()
+                    .getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA);
+
+            keyvalue = applicationInfo.metaData.getString("WeatherAPI_KEY");
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return keyvalue;
+    }
+
+}
---
+++ app/src/main/java/com/jxx/weatherforecast/activity/MainActivity.java
@@ -0,0 +1,18 @@
+package com.jxx.weatherforecast.activity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.jxx.weatherforecast.R;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String key = getWeatherKey();
+        Log.d("WeatherAPI",key);
+    }
+}
