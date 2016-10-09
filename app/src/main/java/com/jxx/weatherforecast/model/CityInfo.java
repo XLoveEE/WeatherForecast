7a5606dfc1814317e6c3b3e5962f12105e4ab540
Jxx <27117821@qq.com>
2016-10-09 22:28:17 +0800

    add city model

---
+++ app/src/main/java/com/jxx/weatherforecast/model/CityInfo.java
@@ -0,0 +1,62 @@
+package com.jxx.weatherforecast.model;
+
+/**
+ * Created by jxx on 2016/10/9.
+ */
+
+public class CityInfo {
+    private String city;
+    private String cnty;
+    private String id;
+    private double lat;
+    private double lon;
+    private String prov;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCnty() {
+        return cnty;
+    }
+
+    public void setCnty(String cnty) {
+        this.cnty = cnty;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+
+    public String getProv() {
+        return prov;
+    }
+
+    public void setProv(String prov) {
+        this.prov = prov;
+    }
+}
